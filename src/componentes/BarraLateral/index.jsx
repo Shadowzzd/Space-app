a3095ca3f93847fefe335339c715edcc761f38b2
Guilherme Bartholo <137460791+Shadowzzd@users.noreply.github.com>
2023-10-15 18:34:24 -0300

    Update index.jsx

--- src/componentes/BarraLateral/index.jsx
+++ src/componentes/BarraLateral/index.jsx
@@ -1,5 +1,5 @@
 import styled from "styled-components"
-import ItemNavegacao from "./itemNavegacao"
+import ItemNavegacao from "./ItemNavegacao"
 
 const ListaEstilizada = styled.ul`
     list-style: none;
